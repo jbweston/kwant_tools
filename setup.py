61912c0da34a34f4a161ee44f8047dc6275016f3
Joseph Weston <joseph.weston08@gmail.com>
2015-08-01 11:08:43 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Kwant Tools',
+      version='0.1',
+      description='Useful tools for working with Kwant',
+      author='Joseph Weston',
+      author_email='me@josephweston.org',
+      packages=['kwant_tools'],
+     )
